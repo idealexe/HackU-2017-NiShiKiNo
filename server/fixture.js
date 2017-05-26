e21c89ca81e7201e2802ad103e6440ec8051018a
monmee <jazzmonmee@gmail.com>
2017-05-27 01:27:11 +0900

    indent fixture.js

--- server/fixture.js
+++ server/fixture.js
@@ -12,51 +12,54 @@ if(Threads.find().count()===0){
       isClosed: false,
       threadStatus: 'processing',
       searchRange:{}
-    });
-    console.log(ids);
-    Posts.insert(
-      {
-        postID:'1',
-        threadID:ids,
-        postUser:{},
-        postDate:'2017/05/26 09:00',
-        replyID:{},
-        postComment: 'カレー入れるアレありまぁす!!!'
-      }
-    );
-    ids=Threads.insert(
-      {
-        threadID: '2',
-        customerID: {},
-        threadDate: '2017/05/24',
-        threadTitle:'シンセのACアダプター',
-        threadCategories:{},
-        threadComment:`〇〇の機種に合うACアダプター売ってませんか?`,
-        location:{},
-        isClosed: true,
-        threadStatus:'done',
-        searchRange:{}
-      });
-      Posts.insert(
-        {
-          postID:'2',
-          threadID:ids,
-          postUser:{},
-          postDate:'2017/05/26 10:00',
-          replyID:{},
-          postComment: 'アダプター，うちで売ってます．'
-        }
-      );
-      console.log(ids);
     }
-
-if(Records.find().ocunt() === 0) {
-  var userData = {
-    userID: 1,
-    username: '西木野',
-    address: '東京都調布市',
-    image: '',
-    comment: 'よろしくお願いします。'
-  }
+  );
+  console.log(ids);
+  Posts.insert(
+    {
+      postID:'1',
+      threadID:ids,
+      postUser:{},
+      postDate:'2017/05/26 09:00',
+      replyID:{},
+      postComment: 'カレー入れるアレありまぁす!!!'
+    }
+  );
+  ids=Threads.insert(
+    {
+      threadID: '2',
+      customerID: {},
+      threadDate: '2017/05/24',
+      threadTitle:'シンセのACアダプター',
+      threadCategories:{},
+      threadComment:`〇〇の機種に合うACアダプター売ってませんか?`,
+      location:{},
+      isClosed: true,
+      threadStatus:'done',
+      searchRange:{}
+    }
+  );
+  Posts.insert(
+    {
+      postID:'2',
+      threadID:ids,
+      postUser:{},
+      postDate:'2017/05/26 10:00',
+      replyID:{},
+      postComment: 'アダプター，うちで売ってます．'
+    }
+  );
+  console.log(ids);
 
 }
+//if(Records.find().count() === 0) {
+  //var userids = Records.insert({
+    //userID: {},
+    //username: 'Kiri Man',
+    //address: 'Tokyo, Japan',
+    //image: '',
+    //comment: 'Hi! I am Kiri man'
+  //});
+
+  //console.log(userids);
+//}
