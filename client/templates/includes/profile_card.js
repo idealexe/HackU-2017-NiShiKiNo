f60deb3ddaca47c55068cf730e08cb6e1163c1c6
monmee <jazzmonmee@gmail.com>
2017-05-27 07:07:50 +0900

    初期化ヘルパー関数作成

--- client/templates/includes/profile_card.js
+++ client/templates/includes/profile_card.js
@@ -1,13 +1,28 @@
-import '../../../lib/collections/profile.js'
+if(Records.find().count() === 0) {
+  
+  var profile = {
+    userID: Meteor.userId(),
+    username: 'kiri man',
+    address: 'tokyo',
+    comment: 'Hi! I am Kiri man.'
+  }    
 
-Meteor.call('updateProfile', function(error, result) {
-  if(error) alert('Error');
-  else console.log(result);
-});
-Template.profileCard.helpers({
-    name: "Kiri Man",
-    age: 24,
-    address: "Toky, Japan",
-    comment: "Hi! I am Kiri man.",
-    id: Meteor.userId()
-});
+  Template.profileCard.helpers({
+    profileData: function() {
+      return Template.instance().myAsyncValue.get();
+    }
+  });
+
+  Template.profileCard.created = function (){
+    var self = this;
+    self.myAsyncValue = new ReactiveVar("Waiting for response from server...");
+    Meteor.call('initProfile', profile, function(error, result) {
+      if(error) {
+        alert('Error');
+      } else {
+    	  var profile = Records.findOne({_id: result});
+    	  self.myAsyncValue.set(profile);
+  	  }
+    });
+  }
+}
